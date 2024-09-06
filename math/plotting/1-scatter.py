9f6dc12e054af343cfc5c3c0771517d8110350ab
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2024-09-06 21:01:39 +0200

    Task 1

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -12,4 +13,7 @@ plt.scatter(x, y, c='magenta', s=10)
 plt.title('Men\'s Height vs Weight')
 plt.xlabel('Height (in)')
 plt.ylabel('Weight (lbs)')
+plt.savefig(
+    f"plots/{os.path.basename(__file__)[0:-3] + '_plot.png'}"
+)
 plt.show()
