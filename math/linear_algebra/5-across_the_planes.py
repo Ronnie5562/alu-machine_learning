990cdb32ea6ba6b798d6e01acab32300966d920f
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2024-09-14 20:29:00 +0200

    Task 5

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,13 @@
+def add_matrices2D(mat1, mat2):
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+        return None
+    output_matrix = []
+
+    for i in range(len(mat1)):
+        current_row = [
+            mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))
+        ]
+
+        output_matrix.append(current_row)
+
+    return output_matrix
