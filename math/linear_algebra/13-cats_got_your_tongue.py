4be37b9ca05d886c377382e3308bef0a6c885a80
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2024-09-14 21:06:29 +0200

    Task 13

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -6,4 +6,14 @@ import numpy as np
 
 
 def np_cat(mat1, mat2, axis=0):
+    """_summary_
+
+    Args:
+        mat1 (_type_): _description_
+        mat2 (_type_): _description_
+        axis (int, optional): _description_. Defaults to 0.
+
+    Returns:
+        _type_: _description_
+    """
     return np.concatenate((mat1, mat2), axis=axis)
