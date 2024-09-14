b0ce541a92a134391f0378bd250c38c39f334130
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2024-09-14 20:53:41 +0200

    Task 5

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
+"""_summary_
+Contains a func add_matrices2D(mat1, mat2) that adds 2D matrices
+"""
+
 
 def add_matrices2D(mat1, mat2):
+    """_summary_
+
+    Args:
+        mat1 (_type_): _description_
+        mat2 (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
     output_matrix = []
