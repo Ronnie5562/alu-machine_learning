9fd74ed3babed84d9b5526e44e89e5701837d774
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2024-09-14 20:47:55 +0200

    Task 3

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
+"""_summary_
+Contains a func matrix_transpose(matrix) that returns the transpose of a matrix
+"""
+
 
 def matrix_transpose(matrix):
+    """_summary_
+
+    Args:
+        matrix (_type_): _description_
+
+    Returns:
+        _type_: A list of lists
+    """
     new_matrix = [
         [0 for _ in range(len(matrix))] for _ in range(len(matrix[0]))
     ]
