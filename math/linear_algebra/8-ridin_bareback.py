81f7a33caf6c17527e54c6f4f7c6ba47148526bb
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2024-09-14 20:53:52 +0200

    Task 8

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
+"""_summary_
+Contains a func mat_mul(mat1, mat2) that multiplies two matrices
+"""
+
 
 def mat_mul(mat1, mat2):
+    """_summary_
+
+    Args:
+        mat1 (_type_): _description_
+        mat2 (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     if len(mat1[0]) == len(mat2):
         mat2_T = list(zip(*mat2))
 
