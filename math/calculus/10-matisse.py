a672ed2a431b352d15a3c5a8651c488344e1e898
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2024-10-04 21:06:34 +0200

    Task 10

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""_summary_
+This file contains the implementation of poly_derivative
+"""
+
+
+def poly_derivative(poly):
+    """_summary_
+    Computes the coefficients of the terms in the
+    derivative of a function using Sum rule
+    """
+    lgth = len(poly)
+    if not isinstance(poly, list) or lgth <= 1:
+        return [0]
+
+    derivatives = []
+    for term in enumerate(poly):
+        if term[0] == 0:
+            continue
+        derivatives.append(term[0] * term[1])
+
+    return derivatives
